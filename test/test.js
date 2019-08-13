f8c82cea3687e25a7552ee38b80836af2732a179
Logan Lohman <logan.lohman@gmail.com>
2019-08-12 20:32:06 -0600

    add another test

--- test/test.js
+++ test/test.js
@@ -4,4 +4,8 @@ describe('Tests', () => {
     it('says Hello World', () => {
         assert.equal('hello world', 'hello world');
     });
+
+    it('fails to work', () => {
+        assert.equal('hello', 'bye');
+    });
 });
