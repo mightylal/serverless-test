31e02baeccf3f905cca1240e01e62b02d3e2b347
Logan Lohman <logan.lohman@gmail.com>
2019-09-24 07:56:41 -0600

    more changes

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const bodyParser = require('body-parser-graphql');
+const aws = require('aws-sdk');
+
+const lambda = new aws.Lambda({
+    region: 'us-west-2',
+});
+
+const invoke = lambda.invoke({
+    FunctionName: 'my-express-application-users-dev-users',
+    Payload: JSON.stringify({test: 'This is some data'}),
+});
+
+const test = invoke.promise();
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -17,8 +29,13 @@ var schema = buildSchema(`
 
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-        return 'Hello world!';
+    hello: async () => {
+        const what = await test;
+        const data = JSON.parse(what.Payload.toString());
+        console.log('the waited for function', what, data);
+        console.log('something else', data['result']);
+        console.log('some poop', data[0].poop);
+        return what.Payload.toString();
     },
 };
 
